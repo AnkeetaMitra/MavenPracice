eadeeb397cbaab8c2b40be2d4c3fa445dcf8e29f
Ximanta <ximanta.sarma@gmail.com>
2022-03-25 11:27:56 +0530

    Running code with mvn exec java

---
+++ src/main/java/guru/springframework/EvenSum.java
@@ -0,0 +1,23 @@
+package guru.springframework;
+public class EvenSum {
+
+    public static void main(String[] args) {
+        System.out.println("Inside main");
+//        int num = Integer.parseInt(args[0]);
+//        int[] even = new int[num];
+//        int sum = 0, j = 0;
+//        String evennums = "";
+//        //Insert your code here
+//        for (j = 0; j < num; j++) {
+//            if (num % 2 == 0) {
+//                even[j] = num;
+//                sum = sum + num;
+//                args[j] = Integer.toString(num);
+//            }
+//            evennums = String.join(",", args);
+//        }
+//
+//        System.out.println(evennums);
+//        System.out.println(sum);
+    }
+}  
